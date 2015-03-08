f34e4dfcfd72e902f321a2d657b9e7779c9a5398
A.O.Richardson Jr. <madtech@bellsouth.net>
2015-03-08 13:25:11 -0400

    Character change in engine.js
    fix character change

--- js/engine.js
+++ js/engine.js
@@ -172,7 +172,9 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png'
+        'images/char-cat-girl.png',
+        'images/char-boy.png',
+        
     ]);
     Resources.onReady(init);
 
